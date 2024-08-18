5ef6cc4c76d2731879081d1944004323d705ca80
sekhar <sekharhema0001@gmail.com>
2024-08-18 22:13:22 +0530

    first commit

---
+++ src/com/ex/Hi.java
@@ -0,0 +1,10 @@
+package com.ex;
+
+public class Hi 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hi...");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Windows
+ *
+ */
+module Demo_Examp1 {
+}
